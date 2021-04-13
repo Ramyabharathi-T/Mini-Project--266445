4d3bfa800963a84dad01e9a686b031e05754899a
Rahul-S-Iyer <80777191+Rahul-S-Iyer@users.noreply.github.com>
2021-04-13 22:32:34 +0530

    Add files via upload

---
+++ 3_Implementation/Src/a.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+
+
+void show_record()
+    {system("cls");
+	char name[20];
+	float scr;
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&name,&scr);
+	printf("\n\n\t\t*************************************************************");
+	printf("\n\n\t\t %s has secured the Highest Score %0.2f",name,scr);
+	printf("\n\n\t\t*************************************************************");
+	fclose(f);
+	getch();}
