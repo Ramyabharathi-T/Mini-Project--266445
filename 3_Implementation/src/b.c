4366148b964d35a335e77bf9b3e09313cf4e4495
Rahul-S-Iyer <80777191+Rahul-S-Iyer@users.noreply.github.com>
2021-04-15 12:55:52 +0530

    Create b.c

---
+++ 3_Implementation/src/b.c
@@ -0,0 +1,13 @@
+#include "prototype.h"
+
+
+void reset_score()
+    {system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%s%f",&nm,&sc);
+	sc=0;
+	fprintf(f,"%s,%.2f",nm,sc);
+    fclose(f);}
